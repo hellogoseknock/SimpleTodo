0346a60a0fa13f9deaa75cc0739c775943040815
sof <jump2void@gmail.com>
2024-04-23 00:28:15 +0900

    화면 구성과 버튼 클릭시 입력값 배열에 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 // import './App.css'
+import { useState } from 'react'
 import { Button } from './components/ui/button'
 
 function App() {
 
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const [todoList, setTodoList] = useState<string[]>([])
+  const addItem = () => {
+    console.log(inputValue)
+    setTodoList([...todoList, inputValue])
+  }
 
   return (
     <>
-      <div>
-        <Button>hello from macbook</Button>
-      </div>
-      <div>
-        <Button>hello chadcn button</Button>
+      <div className='flex py-5 justify-center gap-2'>
+        <input type="text" name="남길글" placeholder="남길 글" onChange= {e => setInputValue(e.target.value)} className='border border-stone-800 rounded-md' />
+        <Button onClick={addItem}>hello from macbook</Button>
+
       </div>
     </>
   )
